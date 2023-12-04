c5c56b9ddaf63f2f95d393e372b64830cc82e7a8
atborg <33327789+atborg@users.noreply.github.com>
2023-12-04 13:10:28 -0500

    inline condition for query builder as there may be more in future

--- code/elastic.py
+++ code/elastic.py
@@ -46,16 +46,13 @@ def get_documents(doc_ids: list):
 
 def search(domains: list, term: str, page: int=1):
     # TODO: 'term' could be multiple tokens and the search is now a disjunction
-    if not domains:
-        query = {'multi_match': {'query': term, "fields": config.SEARCH_FIELDS}}
-    else:
-        # Using "must" instead of "should". With the latter, documents with scores
-        # of zero were making it into the response.
-        query = {
-            "bool": {
-                "must": {
-                    "multi_match": {"query": term, "fields": config.SEARCH_FIELDS}},
-                "filter": {"terms": {"topic": domains} }}}
+    # Using "must" instead of "should". With the latter, documents with scores
+    # of zero were making it into the response.
+    query = {
+        "bool": {
+            "must": {
+                "multi_match": {"query": term, "fields": config.SEARCH_FIELDS}},
+            "filter": {"terms": {"topic": domains} } if domains else None}}
     # offset for documents returned
     skip = config.MAX_RESULTS * (page - 1)
     result = ES.search(index=INDEX, size=config.MAX_RESULTS, query=query, from_=skip)
